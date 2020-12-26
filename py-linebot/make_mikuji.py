500fa45bbd98bde062685b7be5d33e25f535b294
ryunosuke <ryunosuke0305@gmail.com>
2020-12-26 21:03:23 +0900

    debug

--- py-linebot/make_mikuji.py
+++ py-linebot/make_mikuji.py
@@ -3,22 +3,22 @@ from pathlib import Path
 import random
 from dic import dic
 
-def make_mikuji(text,image):
+def make_mikuji(text):
     base_text=['エンジニアおみくじ','縁起の良い言語','デバッグ運','開発環境','技術書','行うべき習慣']
     
     #元画像を読み込んでくる場合
-    
+
     #path='./static/mikuji/base.jpg'
-    #image = Image.open(path)
+    image = Image.open('py-linebot/static/mikuji/base.jpg')
     # image = Image.open("https://cdn.shibe.online/shibes/907fed97467e36f3075211872d98f407398126c4.jpg")
 
     #文字を書きこむ為のオブジェクトが用意されているので取得する
     draw = ImageDraw.Draw(image)
 
     #フォントを決める
-    font = ImageFont.truetype("static/mikuji/AoyagiKouzanTOTF.otf", size=95)
-    font2 = ImageFont.truetype("static/mikuji/AoyagiKouzanTOTF.otf", size=70)
-    font3 = ImageFont.truetype("static/mikuji/AoyagiKouzanTOTF.otf", size=190)
+    font = ImageFont.truetype("py-linebot/static/mikuji/AoyagiKouzanTOTF.otf", size=95)
+    font2 = ImageFont.truetype("py-linebot/static/mikuji/AoyagiKouzanTOTF.otf", size=70)
+    font3 = ImageFont.truetype("py-linebot/static/mikuji/AoyagiKouzanTOTF.otf", size=190)
 
     #描きたい文字のサイズを取得する
     draw_text_width, draw_text_height = draw.textsize(base_text[0], font=font3)
@@ -51,7 +51,7 @@ def make_mikuji(text,image):
     no=random.randint(0,300)
 
     #出来上がった画像を保存する
-    image.save(f"./static/mikuji/result{no}.jpg")
+    image.save(f"py-linebot/static/mikuji/result{no}.jpg")
 
     return f"result{no}.jpg",text[0]
 
