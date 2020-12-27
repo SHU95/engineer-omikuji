f22afdaac539a4ad0cf29844ef0aaeaf8c0f2b07
Kalancs <51254337+Kalancs@users.noreply.github.com>
2020-12-27 14:49:02 +0900

    updated

--- py-linebot/omikuji.py
+++ py-linebot/omikuji.py
@@ -127,8 +127,8 @@ def make_mikuji(text):
     image.save(f"py-linebot/static/mikuji/result{no}.png")
 
     return f"result{no}.png",
-        text[0] + "でした！\n" +
-        "縁起の良い言語は" + text[1] + "でした！\n" + 
+        text[0] + "でした！" +
+        "縁起の良い言語は" + text[1] + "でした！" + 
         "特に" + base_text[3 + (no % 3)] + "は" + {text[3 + (no % 3)]} + "でした！"
 
 
